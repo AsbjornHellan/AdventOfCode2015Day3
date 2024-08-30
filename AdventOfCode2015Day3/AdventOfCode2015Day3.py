de29acab5fa12430cb7d6175b381639f74d1e567
Asbjorn Hellan <asbjornhellan@outlook.com>
2024-08-30 19:07:07 +0200

    # Deliver one present for each character. # Subtract one present for each move forward and backwards.

--- AdventOfCode2015Day3/AdventOfCode2015Day3.py
+++ AdventOfCode2015Day3/AdventOfCode2015Day3.py
@@ -1,4 +1,7 @@
+# How many houses receive at least one present?
 file_input = open('input_2015_day3.txt' , 'r')
+
+
 
 history = []
 
@@ -8,7 +11,41 @@ while 1:
     if not char:
         break
 print(history)
+print(len(history))
 
+dir_north = '^'
+dir_south = 'v'
+dir_east = '>'
+dir_west = '<'
+presents_delivered = 0
 
+# Delive one present for each visit
+for house in history:
+    if house == dir_north or house == dir_south or house == dir_east or house == dir_west:
+        presents_delivered += 1
+
+# Subtract one present for each move forward and backward
+pre_visit = '>'
+for house in history:
+    if house == '^' and pre_visit == 'v':
+        presents_delivered -= 1
+    pre_visit = house
+for house in history:
+    if house == 'v' and pre_visit == '^':
+        presents_delivered -= 1
+    pre_visit = house
+for house in history:
+    if house == '>' and pre_visit == '<':
+        presents_delivered -= 1
+    pre_visit = house
+for house in history:
+    if house == '<' and pre_visit == '>':
+        presents_delivered -= 1
+    pre_visit = house
+
+# Subtract two presents for each square move
+        
+
+print(presents_delivered)
 
 file_input.close()
