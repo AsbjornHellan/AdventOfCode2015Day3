64cb29b0150611d58e918df55284eede545affeb
Asbjorn Hellan <asbjornhellan@outlook.com>
2024-08-30 20:14:38 +0200

    Fix error

--- AdventOfCode2015Day3/AdventOfCode2015Day3.py
+++ AdventOfCode2015Day3/AdventOfCode2015Day3.py
@@ -1,25 +1,28 @@
 # How many houses receive at least one present?
+
 file_input = open('input_2015_day3.txt' , 'r')
-
-
 
 town = []
 
 while 1:
     char = file_input.read(1)
+    print(char)
     town += char
     if not char:
         break
+
 print(town)
 print(len(town))
 
 # Delive one present to each visit
 presents_delivered = 0
+
 for house in town:
         presents_delivered += 1
 
 # Subtract one present for each move backward
 pre_house = '>'
+
 for house in town:
     if pre_house == '>' and house == '<':
         presents_delivered -= 1
@@ -41,7 +44,6 @@ for house in town:
     pre_house = house
 
 # Subtract one presents for each square move
-
 
 print(presents_delivered)
 
